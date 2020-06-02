811809f3095f8f599affcd8dc82b2c3563f89f9e
Pavel Moravec <pmoravec@redhat.com>
2020-06-02 08:09:00 +0200

    use associative arrays to boost performance
    Replace times.* and the "maxpending" files by associative arrays and
    fluh them to times.* at the end. That boosts performance several times.
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- analyze.sh
+++ analyze.sh
@@ -43,12 +43,13 @@ rm -f times.* results.* concur-requests.*
 # ..
 # 2020-04-01T03:17:59 [I|app|07bb6705] Completed 200 OK in 38ms (Views: 10.7ms | ActiveRecord: 7.3ms)
 #
-# relevant data: requestID, processing/completed, type of request or its rudation; in particular:
+# ignore empty request IDs
+# relevant data: requestID, processing/completed, type of request or its duration; in particular:
 #
 # 07bb6705 p Katello::Api::V2::RootController#rhsm_resource_list
 # 07bb6705 c 38ms 
 echo "$(date): extracting relevant data from input file '${f}'.."
-grep -e Processing -e Completed $f | cut -d'|' -f3 | sed "s/\] Completed / c in /g" | sed "s/\] Processing by / p in  in /g" | awk -F " in " '{ print $1" "$3 }' | awk '{ print $1" "$2" "$3 }' > processing.completed.extracted
+grep -e "\] Processing by" -e "\] Completed" $f | cut -d'|' -f3 | grep -v "^]" | sed "s/\] Completed / c in /g" | sed "s/\] Processing by / p in  in /g" | awk -F " in " '{ print $1" "$3 }' | awk '{ print $1" "$2" "$3 }' > processing.completed.extracted
 
 # Check if there are any valid results by looking at processing.completed.extracted or exit
 if [ ! -s processing.completed.extracted ]; then
@@ -62,39 +63,45 @@ fi
 # - if act is "c" / Completed, fetch from "requests" hash the type and append the time (without "ms") to the relevant file
 echo "$(date): processing the extracted data (this can take a while).."
 declare -A requests
+declare -A times
 # also count number of pending requests for stats about level of concurrency
 pending=0
 maxpending=0
-cat processing.completed.extracted | while read req act typeortime; do
+maxpendingid=""
+pendinglist=()
+while read req act typeortime; do
 	if [ "$act" == "p" ]; then
 		requests[${req}]="${typeortime}"
 		pending=$((pending+1))
-		echo $pending >> concur-requests.txt
+		pendinglist+=($pending)
 		if [ ${pending} -gt ${maxpending} ]; then
 			maxpending=${pending}
-			echo ${req} > concur-requests.maxpending.id.txt
+			maxpendingid=${req}
 		fi
 	else
 		type=${requests[${req}]}
-		unset requests[${req}]
 		# if $type is empty, we didnt hash the request that was logged in a previous (missing) logfile - ignore this
 		if [ -n "${type}" ]; then
-			echo ${typeortime%ms} >> times.${type}
+			unset requests[${req}]
+			times[${type}]+="${typeortime%ms},"
+#			echo ${typeortime%ms} >> times.${type}
 			pending=$((pending-1))
-			echo $pending >> concur-requests.txt
+			pendinglist+=($pending)
 		else # increment all past concurrency levels by one
 			maxpending=$((maxpending+1))
-			if [ -f concur-requests.txt ]; then
-				rm -f concur-requests.txt.aux
-				for i in $(cat concur-requests.txt); do
-					echo $((i+1)) >> concur-requests.txt.aux
-				done
-				mv concur-requests.txt.aux concur-requests.txt
-			fi
+#			echo "$(date): ${req} ${act} ${typeortime}: bumping pendinglist of size ${#pendinglist[@]}"
+			for i in ${!pendinglist[@]}; do
+				pendinglist[$i]=$((pendinglist[$i]+1))
+			done
 		fi
 	fi
-done
+done < <(cat processing.completed.extracted)
 rm -f processing.completed.extracted
+
+# write the times to corresponding times.* files
+for key in "${!times[@]}"; do
+        echo ${times[$key]} | tr ',' '\n' > times.${key}
+done
 
 # each file times.${type} now contains response times of requests of the type ${type}; let do some stats over them
 # first, find overall number of requests and their duration and print it to results.summary file
@@ -130,15 +137,15 @@ done
 echo >> results.table
 
 # count stats from concurrent requests processed
+echo ${pendinglist[@]} | tr ' ' '\n' > concur-requests.txt
 sort -n concur-requests.txt > concur-requests.sorted.txt
 read sumconcur countconcur < <(awk '{ sum+=$1; c+=1 } END { print sum" "c }' concur-requests.txt)
 echo "concurrent requests:" > results.concur
-echo "- MAX: $(tail -n1 concur-requests.sorted.txt) when processing request with ID $(cat concur-requests.maxpending.id.txt)" >> results.concur
+echo "- MAX: $(tail -n1 concur-requests.sorted.txt) when processing request with ID '${maxpendingid}'" >> results.concur
 echo "- AVG: $(echo $sumconcur $countconcur | awk '{ printf "%.0f", $1/$2 }')" >> results.concur
 echo "- MEAN: $(head -n $(((countconcur+1)/2)) concur-requests.sorted.txt | tail -n1)" >> results.concur
 echo "- 90%PERCENTILE: $(tail -n $((countconcur/10)) concur-requests.sorted.txt | head -n1)" >> results.concur
 echo >> results.concur
-rm -f concur-requests.maxpending.id.txt
 
 echo > results.footer
 echo "results are in results.* files, individual requests per each type are in times.* files, concurrent requests in concur-requests.txt file" >> results.footer
